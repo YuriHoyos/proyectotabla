224370c687cc9005e5b8aec7fd466678f883b74b
Has <hlozada5@misena.com>
2024-04-02 16:46:25 -0500

    cambio

--- controlador/eliminarProductoController.php
+++ controlador/eliminarProductoController.php
@@ -1,6 +1,6 @@
 <?php
 include ('../modelo/productoDAO.php');
-$productoDAO = new ProductoDAO(null, null, $_POST['id_producto']);
+$productoDAO = new ProductoDAO($_POST['id_producto']);
 $resultado = $productoDAO->eliminarProducto();
 echo ($resultado ? "El producto fue eliminado correctamente.");
 ?>
